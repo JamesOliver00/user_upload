0ab490aaeddc72e07ddb09e11d717bc356099b96
james <james.oliver.sjc@gmail.com>
2023-09-26 20:19:58 +1300

    Added help function and f_ouput function

--- user_upload.php
+++ user_upload.php
@@ -14,9 +14,35 @@ $long_options =
     "help",         // output options with details
 ];
 
+
+// sends output to file handle
+function f_output($output_stream, $s){
+    fwrite($output_stream, $s);
+    return 0;
+}
+
 // prints options and details then exits
 function help(){
+    $directives = 
+    [
+        "-u" => "username of the MYSQL user",
+        "-p" => "password of the MYSQL user",
+        "-h" => "ip of host database. Port can be specified by attaching the port number to the end of the ip seperated by a ':'. Example '<ip>:<port>' ",
+        "--file" => "this is the name of the csv to be parsed",
+        "--create_table" => "this will create a 'users' in the database defined by host -h, then the script will exit",
+        "--dry_run" => "No inserts will be made to the database defined by host -h and the script will execute as normal",
+        "--help" => "Prints possible command line directive and corresponding functions",
+    ];
+    
+    
+    $help = "\r\n[===== Help =====]\r\n\r\n";
+    foreach($directives as $directive => $desc){
+        $help .= "$directive - $desc\r\n";
+    }
+    $help .= "\r\n\r\n";
 
+    f_output(STDOUT, $help);
+    exit();
 }
 
 
