34aeed8f8fc53bb73b0ea7cbb6b45e40bcb1b308
james <james.oliver.sjc@gmail.com>
2023-09-27 02:17:12 +1300

    added --create_table functionality as well as error checks.

--- user_upload.php
+++ user_upload.php
@@ -54,6 +54,8 @@ function main($args){
     $host_ip = "";
     $host_port = "";
     $filename = "";
+    $dbname = 'test';
+    $tablename = "users";
 
     $create_table = false;
     $dry_run = false;
@@ -67,6 +69,7 @@ function main($args){
         $message.= "\r\n\r\n"; 
 
         f_output(STDOUT, $message);
+        return;
     }
 
     foreach($args as $arg => $val){
@@ -78,9 +81,13 @@ function main($args){
                 $pass = $val;
                 break;
             case "h":
-                if (preg_match('/^(\d[\d.]+):(\d+)\b/', $val, $matches)) {
+                if (preg_match('/^([\.a-zA-Z0-9]+):(\d+)\b/', $val, $matches)) {
+                    
                     $host_ip = $matches[1];
                     $host_port = $matches[2];
+                }
+                else{
+                    $host_ip = $val;
                 }
                 $host = $val;
                 break;
@@ -102,6 +109,68 @@ function main($args){
         
     }
 
+    // connect to db
+    $dsn = "mysql:host=" . $host_ip;
+    if($host_port!=''){
+        $dsn .= ";port=" . $host_port;
+    }
+
+    try{
+        $pdo = new PDO($dsn, $username, $pass);
+    }
+    catch(Exception $e){
+        $err = $e -> getMessage()."\n";
+        $message = "\r\n\r\n"; 
+        $message .= "Error: Database connection fail\r\n";
+        $message .= "Message: $err";
+        $message .= "\r\n\r\n"; 
+
+        f_output(STDOUT, $message);
+        exit();
+    }
+
+    
+    //create table
+    if($create_table)
+    {
+       $createDBQ = "CREATE DATABASE IF NOT EXISTS $dbname";
+       $pdo->query($createDBQ);
+       $pdo->query("USE test");
+       $createTableQ = "CREATE TABLE IF NOT EXISTS $tablename (
+            name varchar(255),
+            surname varchar(255),
+            email varchar(255) UNIQUE
+        );";
+        $pdo->query($createTableQ);
+        exit();
+    }
+    else{
+        try{
+            $pdo->query("USE $dbname");
+            $queryRes = $pdo->query("SHOW TABLES FROM $dbname LIKE '$tablename'");
+            $rows = $queryRes->fetchAll();
+            if(!count($rows)){
+                throw new Exception("Error: Table $tablename doesn't exist");
+            }
+        }
+        catch (Exception $e){
+            $message = "\r\n\r\n"; 
+            $message .= "Error: Database not figured correctly\r\n";
+            $message .= "Try, --create_table to resolve database issues";
+            $message .= "\r\n\r\n"; 
+            f_output(STDOUT, $message);
+            exit();
+        }
+    }
+
+    //process csv file
+
+    
+
+    //check dry run
+    if(!$dry_run){
+        
+    }
 }
 
 $args = getopt($short_options, $long_options);
