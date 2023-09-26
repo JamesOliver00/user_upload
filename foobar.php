3757c15aa0fd408428c773437eb53a272e9cd39e
james <james.oliver.sjc@gmail.com>
2023-09-27 04:47:02 +1300

    Added foo bar loop and logic

--- foobar.php
+++ foobar.php
@@ -1,7 +1,19 @@
 <?php
 
 function main(){
-
+    for( $i=1 ; $i<=100 ; $i++){
+        $print = "";
+        if($i % 3 == 0){
+            $print .= 'foo';
+        }
+        if($i % 5 == 0){
+            $print .= 'bar';
+        }
+        if($i % 5 && $i % 3){
+            $print = $i;
+        }
+        echo $print . ",";
+    }
 }
 
 main();
